3604350f0c6c918840b3ea638f5f9f3e0fc03155
fanzheng <montage_fz@sina.com>
2019-07-11 14:14:35 +0800

    - [新增] 了解python的基本类型 - [新增] 熟悉字符串的基本操作

---
+++ 01. 基本类型.py
@@ -0,0 +1,37 @@
+# Number 数字类型
+
+type(1)  # int
+type(1.1)  # float
+type(1 + 1.0)  # float
+type(2 * 3)  # int
+type(2 / 2)  # float
+type(2 // 2)  # int
+
+# bool 布尔类型
+type(True)  # bool
+type(False)  # bool
+bool([])  # False
+bool([1, 2, 3])  # True
+bool({})  # False
+bool({'a': 1})  # True
+bool('')  # False
+bool(None)  # False
+
+# complex 复数
+
+# str 字符串
+# 在pyhton 中 用单引号, 双引号, 和三引号 来表示字符串
+type('hello world')  # str
+print('hello world\npython')
+
+# 在字符串前面加上一个 r, 那么这个字符串就代表原始字符串
+# 如果不加上 r,那么\n将会被转义
+print(r'C:\nginx-1.16.0')
+
+# 字符串操作
+print('hello' + ' world')
+print('hello ' * 3)
+print('hello world'[0])
+print('hello world'.split(' '))  # 将字符串以规定的符号分开,且返回成一个数组
+print('hello world'[0:5])  # 截取字符串
+print('hello world'[6:])
---
+++ 02. 字符串.py
@@ -0,0 +1,24 @@
+# 字符串的拼接
+print('py' + 'Thon')
+
+# 字符串的重复
+print('py ' * 3)
+
+# 链接变量和字面值
+prefix = 'hello'
+print(prefix + ' world')
+
+# 截取某一个字符串
+print(prefix[0])
+
+# 截取某一段字符串
+# 截取索引0~2, 但是不包括2
+print(prefix[0:2])
+# 截取索引2后面的所有
+print(prefix[2:])
+# 截取索引从0~3,但是不包括3
+print(prefix[:3])
+
+# 返回字符串长度  len()  自建函数
+
+len(prefix)
