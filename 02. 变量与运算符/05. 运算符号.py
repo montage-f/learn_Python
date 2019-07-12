bc702ec4ee9f6a1744ad1bd17974d0558dbe3714
fanzheng <montage_fz@sina.com>
2019-07-12 18:37:02 +0800

    - [新增] 了解运算符的作用

--- 02. 变量与运算符/05. 运算符号.py
+++ 02. 变量与运算符/05. 运算符号.py
@@ -1,1 +1,21 @@
 # Created by MonTage_fz at 2019/7/12
+
+
+# 算数运算符
+print('hello ' + 'world')
+
+print(9 - 5)
+
+print(9 * 3)
+
+print(9 / 3)
+
+print(9 // 3)
+
+print(9 % 3)
+
+# 2的2次方
+print(2 ** 2)
+# 2的三次方
+print(2 ** 3)
+
