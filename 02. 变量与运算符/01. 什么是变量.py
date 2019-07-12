570c8032edc551cf3d3c467ec7a02b4a1c3b9ada
fanzheng <montage_fz@sina.com>
2019-07-12 18:22:50 +0800

    - [新增] 值类型不可变, 引用类型可变, 元组不可变

--- 02. 变量与运算符/01. 什么是变量.py
+++ 02. 变量与运算符/01. 什么是变量.py
@@ -1,1 +1,10 @@
 # Created by MonTage_fz at 2019/7/12
+
+A = [1, 2, 3, 4, 6, 7]
+print(A)
+B = [1, 2, 3]
+print(A * 3 + B + A)
+
+# 变量命名用有意义的单词来命名是最好的
+skill = ['新月打击', '苍白之瀑', '月之降临', '月神冲刺']
+print(skill)
--- 02. 变量与运算符/03. 值类型与引用类型.py
+++ 02. 变量与运算符/03. 值类型与引用类型.py
@@ -1,1 +1,22 @@
 # Created by MonTage_fz at 2019/7/12
+
+# int str tuple(不可改变) boolean  是值类型
+# list set dict(可变)  是引用类型
+
+a = [1, 2, 3, 4, 5]
+b = a
+
+print(a)
+print(b)
+
+a[1] = '我被改变了'
+
+print(a)
+print(b)
+
+a = 'hello'
+a += ' world'
+print(a)
+
+# id() 显示变量指向的地址
+id(a)
