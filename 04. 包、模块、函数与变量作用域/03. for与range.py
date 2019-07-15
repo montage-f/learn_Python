a1f80b0e0944f943e33985a22f5b55dc62ca1c0a
fanzheng <montage_fz@sina.com>
2019-07-15 18:24:42 +0800

    - [新增] 了解内部的range函数, 并且尝试不使用循环语句, 获取list里面的内容

--- 04. 包、模块、函数与变量作用域/03. for与range.py
+++ 04. 包、模块、函数与变量作用域/03. for与range.py
@@ -1,1 +1,36 @@
 # Created by MonTage_fz at 2019/7/12
+for a in range(0, 10):
+    print(a)
+
+
+a = list(range(0, 10))
+print(a)
+
+for a in range(0, 10, 2):
+    print(a, end=' | ')
+
+
+for a in range(10, 0, -2):
+    print(a, end=' | ')
+
+
+a = list(range(0, 10))
+
+for i in a:
+    if not i % 2:
+        print(i)
+
+
+for i in a:
+    if i % 2:
+        print(i)
+
+# 取出下面列表中的基数
+i = ['神', 2, '奇', 4, '女', 6, '侠', 8, 9]
+
+for j in range(0, len(i), 2):
+    print(i[j], end=' | ')
+
+
+b = i[0:len(i):2]
+print(b)
