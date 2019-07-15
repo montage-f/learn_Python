9a9f16244265889a338bf06d47d1801b4514013d
fanzheng <montage_fz@sina.com>
2019-07-15 11:49:17 +0800

    -[ 新增] 了解表达式的定义 -[ 新增] 了解运算符的优先级

--- 03. 分支、循环、条件与枚举/01. 什么是表达式.py
+++ 03. 分支、循环、条件与枚举/01. 什么是表达式.py
@@ -1,1 +1,20 @@
 # Created by MonTage_fz at 2019/7/12
+
+# 表达式(Expression) 是运算符(operation) 和操作数(operand) 所构成的"序列"
+# 序列是有顺序的,
+# 多个,且没顺序是不能称为序列的
+
+
+# 将字符串1转换成int类型
+c = int('1') + 2
+print(c)
+
+a = 1
+b = 2
+c = 3
+print(a + b * c)
+
+print(a and b or c)
+print((a or b) and c)
+
+# 先算not, 再算and, 最后算or
--- 03. 分支、循环、条件与枚举/02. 表达式的优先级.py
+++ 03. 分支、循环、条件与枚举/02. 表达式的优先级.py
@@ -1,1 +1,19 @@
 # Created by MonTage_fz at 2019/7/12
+
+# 幂运算: **
+# 正负号: +x  , -x
+# 算数运算符: *, /, //, +, -
+# 比较运算符： <, <=, >, >=, ==, !=
+# 逻辑运算符: not, and, or
+
+a = 1
+b = 2
+c = 2
+not a or b + 2 == c
+
+# 1. b + 2 => 4
+# 2. not a or 4 == c
+# 3. 4 == c => False
+# 4. not a or False
+# 5. False or False
+# 6. False
